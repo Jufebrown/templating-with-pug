9411db008cece32e133c6d7bcda8195d34ac0372
Jufebrown <jufebrown@gmail.com>
2017-03-29 13:17:53 -0500

    added routes for about and inventory

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.set(`view engine`, `pug`)
 app.get(`/`, (req, res, next) => {
   res.render(`index`)
 })
+
+app.get(`/about`, (req, res, next) => {
+  res.render(`about`)
+})
+
+app.get(`/inventory`, (req, res, next) => {
+  res.render(`inventory`)
+})
